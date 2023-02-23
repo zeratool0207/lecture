b1370e9e7a813a2c2c65d02d5e0a0b366c685c19
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-02-24 06:27:00 +0900

    230224 0626 commit

--- script.js
+++ script.js
@@ -57,28 +57,44 @@
 
 // 4강 Arrow Function
 // birthYear => 2037 - birthYear; // 얘를 그대로 붙여줌 ( 함수식 )
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1991);
-console.log(age3);
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
 
-// Arrow 다른 표현
-// 함수 안에서 한번 더 거쳐야 할 때
-const yearsUntilRetirement = birthYear => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    return retirement;
+// // Arrow 다른 표현
+// // 함수 안에서 한번 더 거쳐야 할 때
+// const yearsUntilRetirement = birthYear => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return retirement;
+// }
+
+// console.log( yearsUntilRetirement(1991) ); // 19
+
+// // Arrow 다른 표현
+// // 함수 안에서 한번 더 거쳐야 할 때
+// // 여러개의 parameter ( 매개변수 )를 가질 때
+// const yearsUntilRetirementMany = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log( yearsUntilRetirementMany(1991, 'Jonas') ); // Jonas retires in 19 years
+// console.log( yearsUntilRetirementMany(1988, 'Yong') ); // Yong retires in 16 years
+
+// 5강. 함수 내에서 다른 함수 호출
+
+function cutFruitPieces(fruit) {
+    return fruit * 2; // 여기만 바꾸면 됌!!
 }
 
-console.log( yearsUntilRetirement(1991) ); // 19
-
-// Arrow 다른 표현
-// 함수 안에서 한번 더 거쳐야 할 때
-// 여러개의 parameter ( 매개변수 )를 가질 때
-const yearsUntilRetirementMany = (birthYear, firstName) => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    return `${firstName} retires in ${retirement} years`;
+function fruitProccessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`; 
+    return juice;
 }
 
-console.log( yearsUntilRetirementMany(1991, 'Jonas') ); // Jonas retires in 19 years
-console.log( yearsUntilRetirementMany(1988, 'Yong') ); // Yong retires in 16 years
+console.log(fruitProccessor(2,3));

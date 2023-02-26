21a9f7db76ecfdb37b653fac6383587bb09bff5b
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-02-27 05:52:53 +0900

    0227 js Array proceeding...

--- script.js
+++ script.js
@@ -99,55 +99,124 @@
 // console.log(fruitProccessor(2,3));
 
 // 6강. 함수 강의 복습
-const calcAge = function(birthYear) {
-    return 2037 - birthYear;
-};
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear;
+// };
 
-const yearsUntilRetirementMany = (birthYear, firstName) => {
-    const age = calcAge(birthYear);
-    const retirement = 65 - age;
+// const yearsUntilRetirementMany = (birthYear, firstName) => {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
 
-    if ( retirement > 0 ) {
-        console.log(`${firstName} retires in ${retirement} years`);
-        return retirement;
-    } else {
-        console.log(`${firstName} has been retired!!`);
-        return -1;
-    }
-}
+//     if ( retirement > 0 ) {
+//         console.log(`${firstName} retires in ${retirement} years`);
+//         return retirement;
+//     } else {
+//         console.log(`${firstName} has been retired!!`);
+//         return -1;
+//     }
+// }
 
-console.log(yearsUntilRetirementMany(1991,'Jonas'));
-console.log(yearsUntilRetirementMany(1950,'Yong'));
+// console.log(yearsUntilRetirementMany(1991,'Jonas'));
+// console.log(yearsUntilRetirementMany(1950,'Yong'));
 
-//결과
-//Jonas retires in 19 years
-//Yong has been retired!!
+// //결과
+// //Jonas retires in 19 years
+// //Yong has been retired!!
 
 
-// Function Declaration
-function calcAge(birthYear) {
-    return 2037 - birthYear;
-}
+// // Function Declaration
+// function calcAge(birthYear) {
+//     return 2037 - birthYear;
+// }
 
 // // Function Expression
-const calcAge = function(birthYear) {
-    return 2037 - birthYear;
-};
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear;
+// };
 
 // // Arrow function
-const calcAge = birthYear => 2037 - birthYear;
+// const calcAge = birthYear => 2037 - birthYear;
 
 
 
 
 
 
+// 8강 Array ( 배열 )
+// const friends1 = 'Michael';
+// const friends2 = 'Steven';
+// const friends3 = 'Peter';
 
+// const friends = ['Michael','Steven','Peter'];
+// console.log(friends);
+// // (3) ['Michael', 'Steven', 'Peter']
 
+// const y = new Array(1991, 1994, 2000, 2020);
+// console.log(friends[0]); // Michael
+// console.log(friends[2]); // Peter
 
+// console.log(friends.length); // 3
+// console.log(friends[friends.length -1]); // Peter
 
+// // 배열 변경
+// friends[2] = 'Jay';
+// console.log(friends);
+// // (3) ['Michael', 'Steven', 'Jay']
 
+// const firstName = 'Jonas';
+// const jonas = ['Jonas', 'Schemedtmann', 2037 - 1991, 'teacher', friends];
 
+// console.log(jonas);
+
+// //Exercise
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);   // 47 70 19
+ 
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length -1])];
+// console.log(ages); // (3) [47, 70, 19]
+
+// 9강. Array(배열2)
+const friends = ['Michael','Steven','Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+//Remove elements
+friends.pop();   // Last
+const popped = friends.pop();
+console.log(popped);  // Peter
+console.log(friends); // (3) ['John', 'Michael', 'Steven']
+
+friends.shift();   // first
+console.log(friends);  // (2) ['Michael', 'Steven']
+
+console.log(friends.indexOf('Steven')); // 1
+console.log(friends.indexOf('Bob'));    // -1
+
+friends.push(23);
+console.log(friends.includes('Steven'));    // true
+console.log(friends.includes('Bob'));       // false
+console.log(friends.includes('23'));        // false
+console.log(friends.includes(23));          // true
+
+if ( friends.includes('Steven') ) {
+    console.log('You have a friend called Steven');
+} 
+
+// You have a friend called Steven
 
 
 
